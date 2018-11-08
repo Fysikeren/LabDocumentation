fccd5daae7b7249b4b289287a52466d8d0d7e55f
Mathias <Fysikeren@users.noreply.github.com>
2018-11-08 17:03:36 +0100

    Move the Windows machine to the NBI network

--- Scripts/mountShare.sh
+++ Scripts/mountShare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This mounts the shared folder so that it is accessible to the data processing on this machine
 # sudo mount -t cifs //192.168.8.10/MagnetLogs /mnt/magnetlogs -o user=username,pass=password,dom=domain,vers=1.0
-sudo mount -t cifs //192.168.8.10/Susceptometry /mnt/susceptometry -o user=username,pass=password,dom=domain,vers=1.0
+# sudo mount -t cifs //192.168.8.10/Susceptometry /mnt/susceptometry -o user=username,pass=password,dom=domain,vers=1.0
+sudo mount -t cifs //172.20.3.166/Susceptometry /mnt/susceptometry -o user=username,pass=password,dom=domain,vers=1.0
